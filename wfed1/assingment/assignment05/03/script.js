553d79836001b46b5953cadfe511d17e20e22267
Chris <henri@c-henri.com>
2024-04-24 22:40:46 -0500

    finished and submit assignment05
    finished and submitted assignment05,

---
+++ wfed1/assingment/assignment05/03/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.info(`${document.title}: DOM Loaded`);
+
+    const navMainMenuToggle = document.querySelector(".nav-main-menu-toggle");
+
+    navMainMenuToggle.addEventListener("click", (e) => {
+        e.preventDefault();
+        const ariaControls = navMainMenuToggle.getAttribute("aria-controls");
+        if (navMainMenuToggle.getAttribute("aria-expanded") === "false") {
+            navMainMenuToggle.setAttribute("aria-expanded", "true");
+            navMainMenuToggle.setAttribute("aria-label", "Close menu");
+            navMainMenuToggle.parentElement.querySelector(`#${ariaControls}`).toggleAttribute("hidden");
+        } else {
+            navMainMenuToggle.setAttribute("aria-expanded", "false");
+            navMainMenuToggle.setAttribute("aria-label", "Open menu");
+            navMainMenuToggle.parentElement.querySelector(`#${ariaControls}`).toggleAttribute("hidden");
+        }
+    });
+});
